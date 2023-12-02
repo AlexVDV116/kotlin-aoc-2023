1ce6dd38162993b78853b29b993304875cd1a3bf
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-02 14:28:37 +0000

    created day3

---
+++ src/day3/Day03.kt
@@ -0,0 +1,27 @@
+package day3
+
+import readInput
+
+fun main() {
+    val input = readInput("day1/Day01")
+    val d = Day03(input)
+
+    d.part1()
+    d.part2()
+
+    // test if implementation meets criteria from the description, like:
+//     val testInput = readInput("day1/Day01_test")
+//     check(d.part1() == 142)
+}
+
+class Day03(private val input: List<String>) {
+
+    fun part1(): Int {
+        return 0
+    }
+
+    fun part2(): Int {
+        return 0
+    }
+
+}
