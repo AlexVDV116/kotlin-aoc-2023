11eebc9d6dba9097b00319f8ebe9231220c11f1d
AlexVDV116 <98614832+AlexVDV116@users.noreply.github.com>
2023-12-04 15:37:26 +0000

    created template for each day

---
+++ src/DayTemplate.kt
@@ -0,0 +1,27 @@
+package day
+
+import readInput
+
+fun main() {
+    val input = readInput("day01/Day")
+    val d = Day(input)
+
+    d.part1()
+    //d.part2()
+
+    // test if implementation meets criteria from the description, like:
+//     val testInput = readInput("day1/Day01_test")
+//     check(d.part1() == 142)
+}
+
+class Day(private val input: List<String>) {
+
+    fun part1(): Int {
+        return 0
+    }
+
+    fun part2(): Int {
+        return 0
+    }
+
+}
